48d9649584471981f8f777cefd235026335b83a2
zeeekannan <zeeekannan@gmail.com>
2023-05-07 19:25:40 +0530

    refactoring
    refactoring

--- db-operation-service/src/main/java/com/tiger/db/operation/DbOperationServiceApplication.java
+++ db-operation-service/src/main/java/com/tiger/db/operation/DbOperationServiceApplication.java
@@ -3,9 +3,11 @@ package com.tiger.db.operation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableTransactionManagement
 public class DbOperationServiceApplication {
 
 	public static void main(String[] args) {
--- db-operation-service/src/main/java/com/tiger/db/operation/service/DbServiceImpl.java
+++ db-operation-service/src/main/java/com/tiger/db/operation/service/DbServiceImpl.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.tiger.db.operation.model.PriceDetail;
 import com.tiger.db.operation.repo.DbRepository;
@@ -15,11 +18,13 @@ public class DbServiceImpl implements DbService {
 	DbRepository dbRepository;
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED)
 	public void writeAll(List<PriceDetail> priceDetails) {
 		dbRepository.saveAll(priceDetails);
 	}
 
 	@Override
+	@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ)
 	public List<PriceDetail> readAll() {
 		return dbRepository.findAll();
 	}
